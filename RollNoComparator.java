7d695c5dbe4a66fb761131f87a6d320c5ca8b75a
devindias44 <devindias44@gmail.com>
2025-10-28 21:18:41 -0600

    Delete RollNoComparator.java

--- RollNoComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class RollNoComparator implements Comparator<Students> {
-    @Override
-    public int compare(Students s1, Students s2) {
-        return Integer.compare(s1.getRollno(), s2.getRollno());
-    }
-}
